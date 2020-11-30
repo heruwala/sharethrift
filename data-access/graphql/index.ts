41de619f231c8c3bcbe77665379aa9e5c70b690f
etang93 <eddietang715@gmail.com>
2020-11-30 12:40:02 -0500

    Added authenticated call getServerTime

--- data-access/graphql/index.ts
+++ data-access/graphql/index.ts
@@ -2,7 +2,7 @@ let appInsights = require("applicationinsights");
 appInsights.setup().start();
 appInsights.defaultClient.commonProperties = {
   environment: process.env.WEBSITE_HOSTNAME,
-  functionArea: "graphql"
+  functionArea: "graphql",
 };
 let appInsightsClient = appInsights.defaultClient;
 
@@ -10,25 +10,38 @@ import {
   ApolloServerPlugin,
   GraphQLRequestContext,
   GraphQLRequestListener,
-} from 'apollo-server-plugin-base';
+} from "apollo-server-plugin-base";
 
-import { ApolloServer, gql }  from 'apollo-server-azure-functions';
-import cosmosdbconnect from '../shared/data-sources/cosmos-db/connect';
-import MsalAuth from '../shared/auth/msal';
-import { HttpRequest, Context } from '@azure/functions';
+import { ApolloServer, gql } from "apollo-server-azure-functions";
+import cosmosdbconnect from "../shared/data-sources/cosmos-db/connect";
+import MsalAuth from "../shared/auth/msal";
+import { HttpRequest, Context } from "@azure/functions";
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
   }
+
+  type Mutation {
+    getServerTime: String
+  }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: (parent,args,context) => {
-      return `Hello world! ${JSON.stringify(context)}`
+    hello: (parent, args, context) => {
+      return `Hello world! ${JSON.stringify(context)}`;
+    },
+  },
+  Mutation: {
+    getServerTime: (parent, args, context) => {
+      if (context.validated) {
+        return Date.now();
+      } else {
+        return 0;
+      }
     },
   },
 };
@@ -38,65 +51,71 @@ const resolvers = {
   try {
     await cosmosdbconnect();
   } catch (cosmosDbConnectError) {
-    appInsightsClient.trackEvent({name: "COSMOSDB CONNECTION", properties: {customProperty: "Cannot Connect to Cosmos", details: cosmosDbConnectError}});
-    appInsightsClient.trackException(cosmosDbConnectError)
+    appInsightsClient.trackEvent({
+      name: "COSMOSDB CONNECTION",
+      properties: {
+        customProperty: "Cannot Connect to Cosmos",
+        details: cosmosDbConnectError,
+      },
+    });
+    appInsightsClient.trackException(cosmosDbConnectError);
   }
 })();
 
 // referenced from https://jeffmagnusson.com/post/graphql-apollo-server-plugins-in-typescript
 const appInsightsPlugin = <ApolloServerPlugin & GraphQLRequestListener>{
-
   // Fires whenever a GraphQL request is received from a client.
-  requestDidStart(requestContext:GraphQLRequestContext): GraphQLRequestListener | void{
-    appInsightsClient.trackMetric({name: "apollo-query", value: 1});
+  requestDidStart(
+    requestContext: GraphQLRequestContext
+  ): GraphQLRequestListener | void {
+    appInsightsClient.trackMetric({ name: "apollo-query", value: 1 });
     return this;
   },
   // Fires for graph exceptions
-  didEncounterErrors: function(requestContext:GraphQLRequestContext) {
-    appInsightsClient.trackMetric({name: "apollo-error", value: 1});
-    appInsightsClient.trackException({exception: new Error("Apollo Error")});
-    appInsightsClient.trackException({exception: {category:"Apollo Error", details: requestContext.errors}});
-  }
-  
-}
+  didEncounterErrors: function (requestContext: GraphQLRequestContext) {
+    appInsightsClient.trackMetric({ name: "apollo-error", value: 1 });
+    appInsightsClient.trackException({ exception: new Error("Apollo Error") });
+    appInsightsClient.trackException({
+      exception: { category: "Apollo Error", details: requestContext.errors },
+    });
+  },
+};
 
 const getPlaygroundSetting = () => {
-  if(process.env.APOLLO_PLAYGROUND_VISIBLE === "true" ){
-    if(process.env.APOLLO_PLAYGROUND_ENDPOINT){
-      return {endpoint :process.env.APOLLO_PLAYGROUND_ENDPOINT}
+  if (process.env.APOLLO_PLAYGROUND_VISIBLE === "true") {
+    if (process.env.APOLLO_PLAYGROUND_ENDPOINT) {
+      return { endpoint: process.env.APOLLO_PLAYGROUND_ENDPOINT };
     }
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
-}
+};
 
-const server = new ApolloServer(
-  { 
-    typeDefs, 
-    resolvers, 
-    playground: {endpoint:process.env.APOLLO_PLAYGROUND_ENDPOINT},
-    plugins: [
-      appInsightsPlugin
-    ],
-    context: async (request) => {
-      var [ user, validated ] = await MsalAuth.VerifyAccessToken(request);
-      return {user, validated};
-    },
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: { endpoint: process.env.APOLLO_PLAYGROUND_ENDPOINT },
+  plugins: [appInsightsPlugin],
+  context: async (request) => {
+    var [user, validated] = await MsalAuth.VerifyAccessToken(request);
+    return { user, validated };
   },
-);
+});
 
 const graphqlHandler = server.createHandler({
   cors: {
-    origin: '*',
+    origin: "*",
     credentials: true,
   },
 });
 
 export default (context: Context, req: HttpRequest) => {
   // https://github.com/Azure/azure-functions-host/issues/6013
-  req.headers['x-ms-privatelink-id'] = '';
+  req.headers["x-ms-privatelink-id"] = "";
   // apollo-server only reads this specific string
-  req.headers['Access-Control-Request-Headers'] = req.headers['Access-Control-Request-Headers'] || req.headers['access-control-request-headers'];
+  req.headers["Access-Control-Request-Headers"] =
+    req.headers["Access-Control-Request-Headers"] ||
+    req.headers["access-control-request-headers"];
   return graphqlHandler(context, req);
-}
+};
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {useMsal} from './components/msal-react-lite'
-import HelloWorld from './components/helloWorld';
-import { AuthenticationResult } from '@azure/msal-browser';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useMsal } from "./components/msal-react-lite";
+import HelloWorld from "./components/helloWorld";
+import { AuthenticationResult } from "@azure/msal-browser";
+import { gql, useMutation } from "@apollo/client";
+
+const GET_SERVER_TIME = gql`
+  mutation {
+    getServerTime
+  }
+`;
 
 function App() {
-  const {login,logout,getAuthToken,getAuthResult,isLoggedIn} = useMsal()
-  const [userName,setUsername] = React.useState<string>("");
+  const { login, logout, getAuthToken, getAuthResult, isLoggedIn } = useMsal();
+  const [userName, setUsername] = React.useState<string>("");
+  const [serverTime, setServerTime] = useState<string>("not set");
+  const [getServertime, { loading: updateLanguageLoading }] = useMutation(
+    GET_SERVER_TIME
+  );
+  const getServerTime = async () => {
+    await getServertime()
+      .then((data : any) => {
+        console.log(data);
+        if (data) {
+          setServerTime(data.data.getServerTime.toString());
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   React.useEffect(() => {
-    (async()=>{
-      var authResult = (await getAuthResult())
-      console.log('AuthResult:',authResult);
-      setUsername(((authResult?.idTokenClaims) as any)?.name??'' as string);
-    })()
-  },[isLoggedIn,getAuthResult])
+    (async () => {
+      var authResult = await getAuthResult();
+      console.log("AuthResult:", authResult);
+      setUsername((authResult?.idTokenClaims as any)?.name ?? ("" as string));
+    })();
+  }, [isLoggedIn, getAuthResult]);
 
   return (
     <div className="App">
@@ -25,12 +48,33 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
           {/* ----------Add this below------------- */}
-          <br/>Login Status: {isLoggedIn?<span>Logged In</span> :<span>Logged Out</span>} <br/>
-          {!(userName)?<></>:<>Welcome {userName}!<br/></>}
-          <button onClick={async () =>  await login()}>LogIn</button>
+          <br />
+          Login Status:{" "}
+          {isLoggedIn ? <span>Logged In</span> : <span>Logged Out</span>} <br />
+          {!userName ? (
+            <></>
+          ) : (
+            <>
+              Welcome {userName}!<br />
+            </>
+          )}
+          <button onClick={async () => await login()}>LogIn</button>
           <button onClick={() => logout()}>LogOut</button>
-          <button onClick={async () => console.log('AuthToken:',await getAuthToken())}>Get Auth Token</button>
-          <button onClick={async () => console.log('AuthResult:',await getAuthResult())}>Get Auth Result</button>
+          <button
+            onClick={async () =>
+              console.log("AuthToken:", await getAuthToken())
+            }
+          >
+            Get Auth Token
+          </button>
+          <button
+            onClick={async () =>
+              console.log("AuthResult:", await getAuthResult())
+            }
+          >
+            Get Auth Result
+          </button>
+          <button onClick={() => getServerTime()}>Get Server Time</button>
         </p>
         <HelloWorld />
         <a
@@ -41,6 +85,7 @@ function App() {
         >
           Learn React
         </a>
+          <div>server time: {serverTime}</div>
       </header>
     </div>
   );
