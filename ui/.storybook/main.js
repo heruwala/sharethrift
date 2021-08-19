ab084413bba34ada44f9961f50fd302fe3c552f6
Patrick Gidich <patrick@simnova.com>
2021-08-19 16:20:09 -0400

    adding playwright

--- ui/.storybook/main.js
+++ ui/.storybook/main.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const { setConfig } = require('storybook-addon-playwright/configs');
+const playwright = require('playwright');
 
 module.exports = {
   "stories": [
@@ -6,10 +8,29 @@ module.exports = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "storybook-zeplin/register",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    'storybook-zeplin/register',
+    'storybook-addon-playwright/preset',
+    'storybook-addon-playwright/register',
   ],
+};
 
-}
+(async () => {
+  let browser = {
+    chromium: await playwright["chromium"].launch(),
+    firefox: await playwright["firefox"].launch(),
+    webkit: await playwright["webkit"].launch(),
+  };
+  setConfig({
+    storybookEndpoint: `http://localhost:6006/`,
+    getPage: async (browserType, options) => {
+      const page = await browser[browserType].newPage(options);
+      return page;
+    },
+    afterScreenshot: async (page) => {
+      await page.close();
+    },
+  });
+})();
